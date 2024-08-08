3f72136f55a4c12fff7b987d51112fce99225330
Abin <abin>
2024-08-09 01:15:09 +0530

    Footer layout designed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,16 @@ const Footer = () => {
   return (
     <footer>
       <div className='footer'>
+        <div className='footer-top'>
+          <div>email</div>
+          <div>contact</div>
+        </div>
 
+        <div className='footer-bottom'>
+          <div>about us</div>
+          <div>links</div>
+          <div>follow us</div>
+        </div>
       </div>
     </footer>
   )
